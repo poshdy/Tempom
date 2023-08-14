d1e843d18aca38628f0a78555d38ab1cf8bfcc03
poshdy <roshdy2810@gmail.com>
2023-08-14 05:31:59 -0700

    m

--- tempo/src/components/SideBar.jsx
+++ tempo/src/components/SideBar.jsx
@@ -1,17 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineHome } from "react-icons/ai";
-import { FiLogIn } from "react-icons/fi";
-import { CgProfile } from "react-icons/cg";
-import { BsHeart, BsMenuButton } from "react-icons/bs";
-import { BiHome, BiLogOut, BiSearch, BiSearchAlt } from "react-icons/bi";
-import Searchbar from "./Searchbar";
-import { styles } from "../styles";
-import Login from "./Login";
-import { useSelector } from "react-redux";
-import { BsSuitHeart } from "react-icons/bs";
-import Profile from "../pages/Profile";
-import { MdMic, MdMicExternalOff } from "react-icons/md";
+import { BsHeart } from "react-icons/bs";
+import { BiHome, BiLogOut, BiSearch } from "react-icons/bi";
+
+import { MdMic } from "react-icons/md";
 
 const size = 25;
 const SideBar = () => {
@@ -28,42 +20,60 @@ const SideBar = () => {
   return (
     <aside className="sticky left-0 top-0 h-screen w-[10%] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg">
       {/* <span className="flex md:hidden" onClick={()=>setSearch(!search)}><BsMenuButton size={20}/></span> */}
-      <nav className={` hidden md:flex flex-col items-start justify-around w-full h-full px-5 py-3`}>
-        <h1 className="font-extrabold tracking-tighter leading-tight text-4xl">TEMPO</h1>
+      <nav
+        className={` hidden md:flex flex-col items-start justify-around w-full h-full px-5 py-3`}
+      >
+        <Link to={'/'} className="font-extrabold tracking-tighter leading-tight text-4xl">
+          TEMPO
+        </Link>
         <div className="font-medium text-xl flex items-start gap-1 flex-col">
-          <Link to={'/'}>Browse</Link>
-          <Link to={'/'}>Search</Link>
+          <Link to={"/"}>Browse</Link>
+          <Link to={"/"}>Search</Link>
         </div>
         <div className="font-medium text-xl flex items-start gap-1 flex-col">
           <h1 className="text-sm font-bold text-white/80 ">Libariy</h1>
-          <Link to={'/discover'}>Discover</Link>
-          <Link to={'/discover'}>Genres</Link>
-          <Link to={'/'}>Top Artists</Link>
+          <Link to={"/discover"}>Discover</Link>
+          <Link to={"/discover"}>Genres</Link>
+          <Link to={"/"}>Top Artists</Link>
         </div>
         <div className="font-medium text-xl flex items-start gap-1 flex-col">
           <h1 className="text-sm font-bold text-white/80 ">My Account</h1>
-          <Link to={'/'}>Likes</Link>
-          <Link to={'/'}>Logout</Link>
+          <Link to={"/"}>Likes</Link>
+          <Link to={"/"}>Logout</Link>
         </div>
       </nav>
-      <nav className={`flex flex-col w-full h-full justify-center md:hidden`}> 
-      <div className="font-medium text-xl flex items-start gap-1 flex-col">
-          <Link to={'/'}><BiHome size={20}/></Link>
-          <Link to={'/'} ><BiSearch size={20}/></Link>
+      <nav className={`flex flex-col w-full h-full justify-center md:hidden`}>
+        <div className="font-medium text-xl flex items-start gap-1 flex-col">
+          <Link to={"/"}>
+            <BiHome size={20} />
+          </Link>
+          <Link to={"/"}>
+            <BiSearch size={20} />
+          </Link>
         </div>
         <div className="font-medium text-xl flex items-start gap-1 flex-col">
           {/* <h1 className="text-sm font-bold text-white/80 ">Libariy</h1> */}
-         
-          <Link to={'/'}><MdMic size={20}/></Link>
-          <Link to={'/'}><MdMic size={20}/></Link>
-          <Link to={'/'}><MdMic size={20}/></Link>
+
+          <Link to={"/"}>
+            <MdMic size={20} />
+          </Link>
+          <Link to={"/"}>
+            <MdMic size={20} />
+          </Link>
+          <Link to={"/"}>
+            <MdMic size={20} />
+          </Link>
         </div>
         <div className="font-medium text-xl flex items-start gap-1 flex-col">
           {/* <h1 className="text-sm font-bold text-white/80 ">My Account</h1> */}
-          <Link to={'/'}><BsHeart size={20} /></Link>
-          <Link to={'/'}><BiLogOut size={20}/></Link>
+          <Link to={"/"}>
+            <BsHeart size={20} />
+          </Link>
+          <Link to={"/"}>
+            <BiLogOut size={20} />
+          </Link>
         </div>
-     </nav>
+      </nav>
     </aside>
   );
 };
